8a96fa4613b75a4e4b56d19b2a07fefaf69dfce5
Joaquin Aguirre <joaquinismaelaguirre0@gmail.com>
2026-06-26 21:17:22 -0300

    segundo commit

--- Juegos.py
+++ Juegos.py
@@ -7,3 +7,21 @@ def jugar_ahorcado():
     intentos = 6
     puntos = 0
     puntaje = 0
+
+    print("Bienvenido al Ahorcado")
+    print("las palabras son relacionadas con la programacion")
+    print("Tienes que adivinar la palabra. Tienes 6 intentos.")
+    nombre = input("Ingresa tu nombre jugador: ")
+
+    
+    while intentos > 0 and "_" in adivinado:
+        print("Palabra:", " ".join(adivinado))
+        letra = input("Adivina una letra: ").lower()
+        
+        if letra in palabra:
+            for i, char in enumerate(palabra):
+                if char == letra:
+                    adivinado[i] = letra
+        else:
+            intentos -= 1
+            print(f"Incorrecto. Te quedan {intentos} intentos.")
